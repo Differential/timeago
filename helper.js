cde481d16e7a93cddd0e3395655ac15130063f33
Josh Owens <joshua.owens@gmail.com>
2014-01-23 13:49:33 -0500

    Allow a default wording to be passed in as an option instead of forcing 'some time ago'

--- helper.js
+++ helper.js
@@ -1,10 +1,16 @@
 if (typeof Handlebars !== 'undefined') {
-  Handlebars.registerHelper("timeago", function(date) {
+  Handlebars.registerHelper("timeago", function(date, options) {
+    override = options.hash['default']
     var dateObj;
     if (date) {
       dateObj = new Date(date);
       return $.timeago(dateObj).replace(/\ /g, "\u00a0");
     }
-    return "some time ago";
+    if (override == undefined) {
+      return "some time ago";
+    } else {
+      return override;
+    }
   });
 }
+
